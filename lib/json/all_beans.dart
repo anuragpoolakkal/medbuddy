b7208783f919a07290f4bc9fb0246f22950642c0
Anurag Rajan <anuragrajanp@gmail.com>
2022-02-27 15:56:00 +0530

    planbud files deleted

--- lib/json/all_beans.dart
+++
@@ -1,9 +0,0 @@
-export 'common_bean.dart';
-export 'photo_bean.dart';
-export 'update_info_bean.dart';
-export 'weather_bean.dart';
-export 'login_bean.dart';
-export 'upload_avatar_bean.dart';
-export 'register_bean.dart';
-export 'upload_task_bean.dart';
-export 'cloud_task_bean.dart';
