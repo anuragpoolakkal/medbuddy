019c63cad069a9750acf8e9f0226fd83c59c2893
Dilshith T S <94925244+abnormaldil@users.noreply.github.com>
2022-03-04 11:48:29 +0530

    changes in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:medbuddy/src/global_bloc.dart';
-import 'package:medbuddy/src/ui';
-import 'package:';
-import 'package:';
-import 'package:';
+import 'package:medbuddy/src/ui/splash_screen/splash.dart';
+import 'package:medbuddy/src/ui/tabpage/tabs.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+runApp(MyApp());
+}
+
+class medbuddy extends StatefulWidget {
+    @override
+    _medbuddy createstate() => _medbuddy();
+}
+
+class MyApp extends StatefulWidget {
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp (
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+        primaryswatch: Colors.blue,
+        ),
+        home:Splash(),
+        );
+    }
+}
+class _medbuddy extends State<medbuddy>{
+
+} 
